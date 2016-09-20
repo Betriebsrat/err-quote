8115a4f70a5bcae19681158dac9f82e3487d0ec8
betriebsrat <betriebsrat@drugfactory.org>
2016-09-20 10:08:32 +0200

    added data, author storage, new commands: quote add, quote new, quote get last, switched quote add to admin only

--- Quote.py
+++ Quote.py
@@ -5,7 +5,7 @@ from errbot import botcmd, BotPlugin
 
 
 class Quote(BotPlugin):
-    """Simple sqlite based quote storage"""
+    """Sqlite based quote storage for chat channels"""
 
     def activate(self):
         QUOTE_DB = self.plugin_dir + os.sep + 'quote.sqlite'
@@ -13,10 +13,15 @@ class Quote(BotPlugin):
             logging.warning('no database found, creating a new one')
             open(QUOTE_DB, 'a').close()
 
-        self.con = sqlite3.connect(QUOTE_DB, check_same_thread=False)
-        self.cur = self.con.cursor()
-        self.cur.execute('''create table if not exists quotes (id integer primary key, quote text not null)''')
-        self.con.commit()
+        self.con = None
+        try:
+            self.con = sqlite3.connect(QUOTE_DB, check_same_thread=False)
+            self.cur = self.con.cursor()
+            self.cur.execute(
+                'create table if not exists quotes (id integer primary key, quote text not null, author text default \'unknown\', date text default CURRENT_DATE) ;')
+            self.con.commit()
+        except sqlite3.Error as e:
+            print(e)
         super(Quote, self).activate()
 
     def deactivate(self):
@@ -26,12 +31,24 @@ class Quote(BotPlugin):
     @botcmd()
     def quote(self, msg, args):
         """ Returns random quote, usage: !quote"""
-        self.cur.execute('''select * from quotes order by random() limit 1''')
+        self.cur.execute('select * from quotes order by random() limit 1;')
         quote = self.cur.fetchone()
         if quote is not None:
-            return '[%d] %s' % (quote[0], quote[1])
+            return '[%d] *%s*' % (quote[0], quote[1])
         else:
             return 'Nothing added yet'
+
+    @botcmd()
+    def quote_details(self, msg, args):
+        """ Returns further details about a quote, usage !quote details <id>"""
+        if args.isdigit() == False:
+            return 'Usage = !quote details <id>'
+        self.cur.execute('select * from quotes where id = ' + args + ';')
+        quote = self.cur.fetchone()
+        if quote is not None:
+            return '[%d] *%s*, Author: %s Date: %s' % (quote[0], quote[1], quote[2], quote[3])
+        else:
+            return 'No matches with id %s' % args
 
     @botcmd()
     def quote_find(self, msg, args):
@@ -39,7 +56,7 @@ class Quote(BotPlugin):
         if args == '':
             return "Usage: !quote find <args>"
 
-        self.cur.execute('''select * from quotes where quote like ? order by random() limit 1''', ('%' + args + '%',))
+        self.cur.execute('select * from quotes where quote like ? order by random() limit 1;', ('%' + args + '%',))
         quote = self.cur.fetchone()
         if quote is None:
             return 'Found no matches for: %s.' % (args)
@@ -47,36 +64,46 @@ class Quote(BotPlugin):
             return '[%d] %s' % (quote[0], quote[1])
 
     @botcmd()
-    def quote_add(self, msg, args):
-        """Adds a new quote, usage: !quote add <string> """
-        if args == '':
-            return "Usage: !quote add <args>"
-
-        self.cur.execute('''insert into quotes (quote) values (?)''', (args,))
-        self.con.commit()
-
-        return 'Added: %s.' % (args)
-
-    @botcmd(split_args_with=None, admin_only=True)
-    def quote_del(self, msg, args):
-        """Removes quote from database, usage: !quote del <id>"""
-        if len(args) != 1:
-            return "Usage: !quote del <id>"
-
-        self.cur.execute('''delete from quotes where id = ?''', ([args[0]]),)
-        self.con.commit()
-
-        return 'Removed: %s.' % args[0]
-
-    @botcmd(split_args_with=None)
     def quote_get(self, msg, args):
-        """Fetches quote by ID, usage: !quote get <id>"""
-        if len(args) != 1:
-            return "Usage: !quote get <id>"
-
-        self.cur.execute('''select * from quotes where id = ?''', ([args[0]]),)
+        """Fetches quote by ID or last quote, usage: !quote get <id/last>"""
+        if args == 'last':
+            self.cur.execute('select * from quotes order by id desc;')
+        elif args.isdigit() == True:
+            self.cur.execute('select * from quotes where id = ' + args + ';')
+        else:
+            return 'Usage: !quote get <id>'
         quote = self.cur.fetchone()
         if quote is None:
             return 'No matches with id = %s.' % args[0]
         else:
             return '[%d] %s' % (quote[0], quote[1])
+
+    @botcmd()
+    def quote_new(self, msg, args):
+        """Returns the last 3 quotes, usage !quote new"""
+        if args != '':
+            return 'Usage !quote new'
+        self.cur.execute('select * from quotes order by id desc limit 3;')
+        rows = self.cur.fetchall()
+        for row in rows:
+            yield '[%d] %s' % (row[0], row[1])
+
+    @botcmd(admin_only=True)
+    def quote_add(self, msg, args):
+        """Adds a new quote, usage: !quote add <string> """
+        if args == '':
+            return "Usage: !quote add <args>"
+        author = msg.frm.nick
+        self.cur.execute('insert into quotes (quote, author) values (?,?);', (args, author))
+        self.con.commit()
+        return 'Added: %s.' % (args)
+
+
+    @botcmd(admin_only=True)
+    def quote_del(self, msg, args):
+        """Removes quote from database, usage: !quote del <id>"""
+        if args == '':
+            return "Usage: !quote del <id>"
+        self.cur.execute('delete from quotes where id = ' + args + ';')
+        self.con.commit()
+        return 'Removed: %s.' % args
